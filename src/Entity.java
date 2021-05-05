272678f221cf2b8af84996a7dafcd79829f385de
Alexis Moins <alexis.moins@etu.u-bordeaux.fr>
2021-05-05 08:26:08 +0200

    Reworked Entity and Cell

--- src/Entity.java
+++ src/Entity.java
@@ -1,7 +1,24 @@
-/**
- *
- * @author Alexis Moins
- */
-interface Entity {
-    
+Entity {
+
+    private final char CHARACTER;
+
+    private final Coordinates COORDINATES;
+
+    Entity(int x, int y, char character) {
+       this.COORDINATES = new Coordinates(x, y);
+       this.CHARACTER = character;
+    }
+
+    public void setCharacter(char character) {
+        this.CHARACTER = character;
+    }
+
+    public static Entity box(int x, int x) {
+        return new Entity(x, y, 'C');
+    }
+
+    public static Entity player(int x, int x) {
+        return new Entity(x, y, 'P');
+    }
+
 }
